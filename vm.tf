a51828d73816e2f55a6166e0006219c35ddd0fa6
aps1cloud <aps1@texiio.com>
2023-09-01 11:27:08 +0530

    df

--- vm.tf
+++ vm.tf
@@ -92,8 +92,7 @@ resource "azurerm_subnet_network_security_group_association" "subnet" {
 
 }
   
-# creating windows vm  
-
+# creating windows vm 
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name

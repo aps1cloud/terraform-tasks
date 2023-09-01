095bcadc09872db47246ca569e14d48ad361bf15
aps1cloud <aps1@texiio.com>
2023-09-01 11:42:14 +0530

    df

--- vm.tf
+++ vm.tf
@@ -1,118 +1,118 @@
-provider "azurerm" {
-  features {}
-}
-# creating resource group example with west europe 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources01"
-  location = "West Europe"
-}
-# creating virtual network 
+# provider "azurerm" {
+#   features {}
+# }
+# # creating resource group example with west europe 
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources01"
+#   location = "West Europe"
+# }
+# # creating virtual network 
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-# creating subnet 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-# creating nsg group
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-# denying internet access for azure vm in nsg rule
-resource "azurerm_network_security_rule" "deny_outbound" {
-  name                        = "DenyAllInternet"
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
-}
-# creating network security rule for RDP 
-resource "azurerm_network_security_rule" "allow_rdp" {
-  name                        = "allow-rdp"
-  priority                    = 101
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "3389"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
-}
-
-# creating public ip 
-resource "azurerm_public_ip" "example" {
-  name                = "example-publicip01"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method  = "Dynamic"
-}
-# creating network interface 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.example.id
-    
-  }
-}
-
-# resource "azurerm_network_interface_security_group_association" "nsgassocite" {
-#     network_interface_id = azurerm_network_interface.example.id
-#     network_security_group_id = azurerm_network_security_group.example.id
-  
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-network"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+# # creating subnet 
+# resource "azurerm_subnet" "example" {
+#   name                 = "internal"
+#   resource_group_name  = azurerm_resource_group.example.name
+#   virtual_network_name = azurerm_virtual_network.example.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
+# # creating nsg group
+# resource "azurerm_network_security_group" "example" {
+#   name                = "example-nsg"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+# # denying internet access for azure vm in nsg rule
+# resource "azurerm_network_security_rule" "deny_outbound" {
+#   name                        = "DenyAllInternet"
+#   priority                    = 100
+#   direction                   = "Outbound"
+#   access                      = "Deny"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.example.name
+#   network_security_group_name = azurerm_network_security_group.example.name
+# }
+# # creating network security rule for RDP 
+# resource "azurerm_network_security_rule" "allow_rdp" {
+#   name                        = "allow-rdp"
+#   priority                    = 101
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_range      = "3389"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = azurerm_resource_group.example.name
+#   network_security_group_name = azurerm_network_security_group.example.name
 # }
 
-# associating nsg to subnet
-resource "azurerm_subnet_network_security_group_association" "subnet" {
-    network_security_group_id = azurerm_network_security_group.example.id
-    subnet_id = azurerm_subnet.example.id
-
-}
+# # creating public ip 
+# resource "azurerm_public_ip" "example" {
+#   name                = "example-publicip01"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   allocation_method  = "Dynamic"
+# }
+# # creating network interface 
+# resource "azurerm_network_interface" "example" {
+#   name                = "example-nic"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
   
-# creating windows vm 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.example.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id = azurerm_public_ip.example.id
+    
+#   }
+# }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-}
+# # resource "azurerm_network_interface_security_group_association" "nsgassocite" {
+# #     network_interface_id = azurerm_network_interface.example.id
+# #     network_security_group_id = azurerm_network_security_group.example.id
+  
+# # }
+
+# # associating nsg to subnet
+# resource "azurerm_subnet_network_security_group_association" "subnet" {
+#     network_security_group_id = azurerm_network_security_group.example.id
+#     subnet_id = azurerm_subnet.example.id
+
+# }
+  
+# # creating windows vm 
+# resource "azurerm_windows_virtual_machine" "example" {
+#   name                = "example-machine"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   size                = "Standard_F2"
+#   admin_username      = "adminuser"
+#   admin_password      = "P@$$w0rd1234!"
+#   network_interface_ids = [
+#     azurerm_network_interface.example.id,
+#   ]
+
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2016-Datacenter"
+#     version   = "latest"
+#   }
+# }
